0f08b9a00dbac04433dc08dad4a6da0a201af531
AlexKnauth <alexander@knauth.org>
2017-02-18 17:17:39 -0800

    split more board-specific functions into board.rkt

--- tic-tac-toe/board.rkt
+++ tic-tac-toe/board.rkt
@@ -1,9 +1,14 @@
-#lang sweet-exp typed/racket/base
+#lang postfix-dot-notation sweet-exp typed/racket/base
 
 provide (all-defined-out)
 
-require my-cond/iffy
+require racket/match
+        racket/list
+        syntax/parse/define
+        my-cond/iffy
         "utils/case-type/case-type.rkt"
+        for-syntax racket/base
+                   syntax/parse
 
 define-type X 'X #:omit-define-syntaxes
 define-type O 'O #:omit-define-syntaxes
@@ -35,4 +40,112 @@ define all-poss
   for*/list ([y (in-range 3)]
              [x (in-range 3)]) : (Listof Pos)
     p(x y)
+
+;; ------------------------------------------------------------------------
+
+: board-winner : Board -> (U Side #f)
+define board-winner(board)
+  define 0-0
+    hash-ref board p(0 0) #f
+  define 1-1
+    hash-ref board p(1 1) #f
+  define 2-2
+    hash-ref board p(2 2) #f
+  or
+    and 0-0
+      or
+        and (equal? 0-0 (hash-ref board '(0 1) #f))
+          (equal? 0-0 (hash-ref board '(0 2) #f))
+          0-0
+        and (equal? 0-0 (hash-ref board '(1 0) #f))
+          (equal? 0-0 (hash-ref board '(2 0) #f))
+          0-0
+        and (equal? 0-0 1-1)
+          (equal? 0-0 2-2)
+          0-0
+    and 1-1
+      or
+        and (equal? 1-1 (hash-ref board '(0 1) #f))
+          (equal? 1-1 (hash-ref board '(2 1) #f))
+          1-1
+        and (equal? 1-1 (hash-ref board '(1 0) #f))
+          (equal? 1-1 (hash-ref board '(1 2) #f))
+          1-1
+        and (equal? 1-1 (hash-ref board '(0 2) #f))
+          (equal? 1-1 (hash-ref board '(2 0) #f))
+          1-1
+    and 2-2
+      or
+        and (equal? 2-2 (hash-ref board '(0 2) #f))
+          (equal? 2-2 (hash-ref board '(1 2) #f))
+          2-2
+        and (equal? 2-2 (hash-ref board '(2 0) #f))
+          (equal? 2-2 (hash-ref board '(2 1) #f))
+          2-2
+
+
+
+: board-full? : Board -> Boolean
+define board-full?(board)
+  {9 <= board.hash-count}
+
+;; ------------------------------------------------------------------------
+
+: transform-board : Board Transform -> Board
+define transform-board(board t)
+  for/hash ([([k : Pos] [v : Side]) (in-hash board)]) : Board
+    values[k.t v]
+
+define-simple-macro
+  sym-trans expr
+  #:with [x-id y-id]
+  syntax-local-introduce #'[x y]
+  λ ([pos : Pos])
+    match-define list(x-id y-id) pos
+    assert expr pos?
+
+: symetry-transforms : (Listof Transform)
+define symetry-transforms
+  list
+    sym-trans list(   x       y   )
+    sym-trans list({2 - x}    y   )
+    sym-trans list(   x    {2 - y})
+    sym-trans list({2 - x} {2 - y})
+    sym-trans list(   y       x   )
+    sym-trans list({2 - y}    x   )
+    sym-trans list(   y    {2 - x})
+    sym-trans list({2 - y} {2 - x})
+
+define-type Transform [Pos -> Pos]
+
+: board=?/symetry : Board Board -> Boolean
+define board=?/symetry(b1 b2)
+  and
+    {b1.hash-count = b2.hash-count}
+    for/or ([t (in-list symetry-transforms)]) : Boolean
+      equal? transform-board(b1 t) b2
+
+;; ------------------------------------------------------------------------
+
+: display-board : Board -> Void
+define display-board(board)
+  displayln " ------- "
+  for ([y in-range(3)])
+    display " | "
+    for ([x in-range(3)])
+      display (hash-ref board (assert list(x y) pos?) (λ () " "))
+    display " | "
+    newline()
+  displayln " ------- "
+
+;; ------------------------------------------------------------------------
+
+: spaces->board : (Listof (Listof (U Side #f))) -> Board
+define spaces->board(spaces)
+  unless {spaces.length = 3}
+    error('spaces->board "bad")
+  for/hash ([pos in-list(all-poss)]
+            [spc in-list(append*(spaces))]
+            #:when spc) : Board
+    values(pos assert(spc))
 
--- tic-tac-toe/tic-tac-toe.rkt
+++ tic-tac-toe/tic-tac-toe.rkt
@@ -256,87 +256,7 @@ define symetry-trim(lst)
         rnd-elt(fst-ish)
         symetry-trim(rst*)
 
-: transform-board : Board Transform -> Board
-define transform-board(board t)
-  for/hash ([([k : Pos] [v : Side]) (in-hash board)]) : Board
-    values[k.t v]
-
-define-simple-macro
-  sym-trans expr
-  #:with [x-id y-id]
-  syntax-local-introduce #'[x y]
-  λ ([pos : Pos])
-    match-define list(x-id y-id) pos
-    assert expr pos?
-
-: symetry-transforms : (Listof Transform)
-define symetry-transforms
-  list
-    sym-trans list(   x       y   )
-    sym-trans list({2 - x}    y   )
-    sym-trans list(   x    {2 - y})
-    sym-trans list({2 - x} {2 - y})
-    sym-trans list(   y       x   )
-    sym-trans list({2 - y}    x   )
-    sym-trans list(   y    {2 - x})
-    sym-trans list({2 - y} {2 - x})
-
-define-type Transform [Pos -> Pos]
-
-: board=?/symetry : Board Board -> Boolean
-define board=?/symetry(b1 b2)
-  and
-    {b1.hash-count = b2.hash-count}
-    for/or ([t (in-list symetry-transforms)]) : Boolean
-      equal? transform-board(b1 t) b2
-
-: board-winner : Board -> (U Side #f)
-define board-winner(board)
-  define 0-0
-    hash-ref board p(0 0) #f
-  define 1-1
-    hash-ref board p(1 1) #f
-  define 2-2
-    hash-ref board p(2 2) #f
-  or
-    and 0-0
-      or
-        and (equal? 0-0 (hash-ref board '(0 1) #f))
-          (equal? 0-0 (hash-ref board '(0 2) #f))
-          0-0
-        and (equal? 0-0 (hash-ref board '(1 0) #f))
-          (equal? 0-0 (hash-ref board '(2 0) #f))
-          0-0
-        and (equal? 0-0 1-1)
-          (equal? 0-0 2-2)
-          0-0
-    and 1-1
-      or
-        and (equal? 1-1 (hash-ref board '(0 1) #f))
-          (equal? 1-1 (hash-ref board '(2 1) #f))
-          1-1
-        and (equal? 1-1 (hash-ref board '(1 0) #f))
-          (equal? 1-1 (hash-ref board '(1 2) #f))
-          1-1
-        and (equal? 1-1 (hash-ref board '(0 2) #f))
-          (equal? 1-1 (hash-ref board '(2 0) #f))
-          1-1
-    and 2-2
-      or
-        and (equal? 2-2 (hash-ref board '(0 2) #f))
-          (equal? 2-2 (hash-ref board '(1 2) #f))
-          2-2
-        and (equal? 2-2 (hash-ref board '(2 0) #f))
-          (equal? 2-2 (hash-ref board '(2 1) #f))
-          2-2
-
-
-
-: board-full? : Board -> Boolean
-define board-full?(board)
-  {9 <= board.hash-count}
-
-
+;; ------------------------------------------------------------------------
 
 : play : Board Side -> Play-Result
 define play(board side)
@@ -361,16 +281,7 @@ define best-move(board side)
     else
       moves.first
 
-: display-board : Board -> Void
-define display-board(board)
-  displayln " ------- "
-  for ([y in-range(3)])
-    display " | "
-    for ([x in-range(3)])
-      display (hash-ref board (assert list(x y) pos?) (λ () " "))
-    display " | "
-    newline()
-  displayln " ------- "
+;; ------------------------------------------------------------------------
 
 : instant-replay : Play-Result -> Play-Result
 define instant-replay(res)
@@ -401,14 +312,6 @@ define instant-replay(res)
     error 'instant-replay "something went wrong because the winner isn't right"
   res
 
-: spaces->board : (Listof (Listof (U Side #f))) -> Board
-define spaces->board(spaces)
-  unless {spaces.length = 3}
-    error('spaces->board "bad")
-  for/hash ([pos in-list(all-poss)]
-            [spc in-list(append*(spaces))]
-            #:when spc) : Board
-    values(pos assert(spc))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
